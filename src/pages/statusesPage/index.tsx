4a90a438e8968bd60b35e9da98d4377110387466
capitandagestan <anyasya1999@gmail.com>
2024-05-14 23:37:57 -0700

    interim commit

--- src/pages/statusesPage/index.tsx
+++ src/pages/statusesPage/index.tsx
@@ -1,28 +1,53 @@
 import './style.css'
 import {useTelegram} from "../../hooks/useTelegram";
-import {useEffect} from "react";
+import {useCallback, useEffect, useState} from "react";
 export const StatusesPage = () => {
     const {tg} = useTelegram();
+    const [subject, setSubject] = useState('Собеседование HR');
 
     useEffect(() => {
-        tg.MainButton.hide();
-    }, []);
+        tg.MainButton.setParams({
+            text: 'Выбрать этап'
+        })
+    }, [])
 
-    const setStatus = (statusText:any) => {
-            const status = {
-                statusText
-            }
-            tg.sendData(JSON.stringify(status));
+
+    const onSendData = useCallback(() => {
+        const data = {
+            statusText: subject
+        }
+        tg.sendData(JSON.stringify(data));
+
+    }, [subject])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
+
+    const onChangeSubject = (e:any) => {
+        setSubject(e.target.value)
     }
     return (
         <div>
             <h3> Выберите этап для кандидатов:</h3>
-                <p onClick={()=>setStatus('Собеседование HR')}>Собеседование HR</p>
-                <p onClick={()=>setStatus('Собеседование Тех')}>Собеседование Тех</p>
-                <p onClick={()=>setStatus('Тестовое задание')}>Тестовое задание</p>
-                <p onClick={()=>setStatus('Служба безопасности')}>Служба безопасности</p>
-                <p onClick={()=>setStatus('Оффер')}>Оффер</p>
-                <p onClick={()=>setStatus('Отказ')}>Отказ</p>
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'Собеседование HR'}>Собеседование HR</option>
+                <option value={'Собеседование Тех'}>Собеседование Тех</option>
+                <option value={'Тестовое задание'}>Тестовое задание</option>
+                <option value={'Служба безопасности'}>Служба безопасности</option>
+                <option value={'Оффер'}>Оффер</option>
+                <option value={'Отказ'}>Отказ</option>
+            </select>
         </div>
 )
 }
