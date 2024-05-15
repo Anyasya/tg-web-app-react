35b7ad11822369a9cc680ce775e7b9df47099707
capitandagestan <anyasya1999@gmail.com>
2024-05-14 23:21:17 -0700

    interim commit

--- src/pages/statusesPage/index.tsx
+++ src/pages/statusesPage/index.tsx
@@ -1,14 +1,29 @@
 import './style.css'
+import {useCallback, useEffect} from "react";
+import {useTelegram} from "../../hooks/useTelegram";
 export const StatusesPage = () => {
+    const {tg} = useTelegram();
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Начать поиск'
+        })
+    }, [])
+
+    const setStatus = (status:any) => {
+            const data = {
+                status
+            }
+            tg.sendData(JSON.stringify(data));
+    }
     return (
         <div>
             <h3> Выберите этап для кандидатов:</h3>
-                <p>Собеседование HR</p>
-                <p>Собеседование Тех</p>
-                <p>Тестовое задание</p>
-                <p>Служба безопасности</p>
-                <p>Оффер</p>
-                <p>Отказ</p>
+                <p onClick={()=>setStatus('Собеседование HR')}>Собеседование HR</p>
+                <p onClick={()=>setStatus('Собеседование Тех')}>Собеседование Тех</p>
+                <p onClick={()=>setStatus('Тестовое задание')}>Тестовое задание</p>
+                <p onClick={()=>setStatus('Служба безопасности')}>Служба безопасности</p>
+                <p onClick={()=>setStatus('Оффер')}>Оффер</p>
+                <p onClick={()=>setStatus('Отказ')}>Отказ</p>
         </div>
 )
 }
